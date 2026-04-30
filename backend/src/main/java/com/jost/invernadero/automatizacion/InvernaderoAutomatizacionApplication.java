e50b7a3ac0778d7708a187fbea14ddf1d82be5aa
John Stiven Sánchez Burbano <jburbano@indigo.tech>
2026-04-29 22:56:25 -0500

    chore: inicializar backend Spring Boot

---
+++ backend/src/main/java/com/jost/invernadero/automatizacion/InvernaderoAutomatizacionApplication.java
@@ -0,0 +1,13 @@
+package com.jost.invernadero.automatizacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvernaderoAutomatizacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvernaderoAutomatizacionApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/jost/invernadero/automatizacion/InvernaderoAutomatizacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jost.invernadero.automatizacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvernaderoAutomatizacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
